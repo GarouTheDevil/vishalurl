59a9dc843db7b08a141da46ce259fb15473c5dac
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-04-11 09:04:52 +0530

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,84 @@
+class Translation(object):
+    START_TEXT = """**This is URL Uploader Bot with Multiple features Please use help button to know about me** 🖥\n\n **Bot Maintained by @MrVishal_2k2**\n\n© @BotDunia"""
+    RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
+    ABS_TEXT = " Please don't be selfish."
+    UPGRADE_TEXT = """**Free Service By @BotDunia**"""
+    FORMAT_SELECTION = "**Select the desired format**: <a href='{}'>**file size might be approximate**</a> \n**If you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons**.\nYou can use /deletethumbnail **to delete the auto-generated thumbnail**."
+    SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
+URL | filename | username | password"""
+    NOYES_URL = "**Sorry link is very slow**."
+    DOWNLOAD_START = "**Trying to download**📥"
+    UPLOAD_START = "**Trying to upload**📤"
+    RCHD_BOT_API_LIMIT = "**Size greater than maximum allowed size (50MB). Neverthless, trying to upload**."
+    RCHD_TG_API_LIMIT = "**Downloaded in** {} **seconds**.\n**Detected File Size**: {}\n**Sorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations**."
+    NOT_AUTH_USER_TEXT = "**Please /upgrade your subscription**."
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "**get lost err 7**"
+    SAVED_CUSTOM_THUMB_NAIL = "**🧿 Custom video / file permanent thumbnail saved. This image will be used in the video / file in This Bot**."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "**Cleared succesfully**."
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "**Media cleared succesfully**."
+    SAVED_RECVD_DOC_FILE = "**Document Downloaded Successfully**."
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_CUSTOM_THUMB_NAIL_FOUND = "**No Custom ThumbNail found**."
+    NO_VOID_FORMAT_FOUND = "**error**\n<b>YouTubeDL</b> said: {}"
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    CURENT_PLAN_DETAILS = "**Premium User Plan**"
+    HELP_USER = """**There are multiple things I can do:
+> Permanent Thumbnail Support For All Features ❤️ (New)
+✳️ All Supported Video / File Formats, with custom file name and custom thumbnail support
+🌀 Upload from any HTTP link, with custom thumbnail support
+✴️ ReName Telegram files, with custom thumbnail support
+✴️ ReName Telegram files as video,with custom Thumbnail Support
+🌀🎦 Convert File To Streamable Video,with Custom Thumbnail Support
+🌀 Convert any Streamable Video to File,with custom Thumbnail Support!!
+🔅 Now u can Mirror Telegram File and get link
+
+Command help for mirroring files
+/getlink1 - File at anonymousfiles.io
+/getlink2 - File at Gofile.io
+/getlink3 - File at Ufile.io
+/getlink4 - File to bayfiles.io(Best)
+
+~~✅JOIN @BotDunia 🎯~~**"""
+    REPLY_TO_DOC_FOR_C2V = "**Reply to a Telegram media to convert**"
+    REPLY_TO_DOC_FOR_SCSS = "**Reply to a Telegram media to get screenshots**"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "**Reply to a Telegram media to /rename with custom thumbnail support**"
+    FF_MPEG_RO_BOT_RE_SURRECT_ED = """Syntax: /trim HH:MM:SS [HH:MM:SS]"""
+    FF_MPEG_RO_BOT_STEP_TWO_TO_ONE = "First send /downloadmedia to any media so that it can be downloaded to my local. \nSend /storageinfo to know the media, that is currently downloaded."
+    FF_MPEG_RO_BOT_STOR_AGE_INFO = "Video Duration: {}\nSend /clearffmpegmedia to delete this media, from my storage.\nSend /trim HH:MM:SS [HH:MM:SS] to cu[l]t a small photo / video, from the above media."
+    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "A saved media already exists. Please send /storageinfo to know the current media details."
+    USER_DELETED_FROM_DB = "User <a href='tg://user?id={}'>{}</a> deleted from DataBase."
+    REPLY_TO_DOC_OR_LINK_FOR_RARX_SRT = "Reply to a Telegram media (MKV), to extract embedded streams"
+    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "**Reply /generatecustomthumbnail to a media album, to generate custom thumbail**"
+    ERR_ONLY_TWO_MEDIA_IN_ALBUM = "**Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album**."
+    INVALID_UPLOAD_BOT_URL_FORMAT = "URL format is incorrect. make sure your url starts with either http:// or https://. You can set custom file name using the format link | file_name.extension"
+    ABUSIVE_USERS = "You have been Banned because @Mrvishal_2k2 has Banned You !!!"
+    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "https://telegram.dog/FFMpegRoBot"
+    EXTRACT_ZIP_INTRO_ONE = "Send a compressed file first, Then reply /unzip command to the file."
+    EXTRACT_ZIP_INTRO_THREE = "Analyzing received file. ⚠️ This might take some time. Please be patient. "
+    UNZIP_SUPPORTED_EXTENSIONS = ("zip", "rar")
+    EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice, and if the issue persists, report this to <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
+    EXTRACT_ZIP_STEP_TWO = """Select file_name to upload from the below options.
+You can use /rename command after receiving file to rename it with custom thumbnail support."""
+    CANCEL_STR = "Process Cancelled"
+    ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
+    FREE_USER_LIMIT_Q_SZE = """Cannot Process.
+Free users only 1 request per 30 minutes.
+/upgrade or Try 1800 seconds later."""
+    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+    FILE_STRG = """👉 <a href="https://t.me/joinchat/AAAAAEl8lVTQP-l8OACwHw">**All Uploaded files are stored here**</a>"""
+    AFTER_SUCCESSFUL_UPLOAD_MSG = """**Uploaded Successful✅**"""
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = """**Uploaded Successful😊✅**"""
+    AFTER_GET_DL_LINK = "**Direct Link generated** 👇\n \n{} \n \n<i>Generated link will expire in {} days.</i> \n\n @BotDunia."
+    ABOUT_ME = """**MY DETAILS:**
+
+```🤖My Name:``` [UploadDuniaBot](https://t.me/UploadDuniaBot)
+    
+```📝 Language:``` [Python 3](https://www.python.org/)
+
+```🧰 Framework:``` [Pyrogram](https://github.com/pyrogram/pyrogram)
+
+```👨🏻‍💻 Developer:``` [MrVishal](https://t.me/Mrvishal_2k2)
+               
+```📢 Channel:``` [BotDunia](https://t.me/BotDunia)
+
+"""
