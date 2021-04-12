b7c6e649d48a7fdfb7c411137abe84b04f4ff9ec
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-04-12 10:52:19 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -1,4 +1,4 @@
- #!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # (c) Shrimadhav U K
 
@@ -42,12 +42,12 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \n<b>◌Progress😉</b>:<code>〘 {2}% 〙</code>\n".format(
-            ''.join(["●" for i in range(math.floor(percentage / 5))]),
-            ''.join(["○" for i in range(20 - math.floor(percentage / 5))]),
+        progress = "[{0}{1}] \n<b>⭕Percentage:</b> {2}%\n".format(
+            ''.join(["□" for i in range(math.floor(percentage / 5))]),
+            ''.join(["■" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "<b>◌Done✓</b><code>〘{0}〙</code>\n<b>◌Total🤯:</b><code>〘{1}〙</code>\n<b>◌Speed🚀</b>:<code>〘 {2}/s 〙</code>\n<b>◌Time Left⏳</b>:<code>〘 {3} 〙</code>\n\n<b>© @David_Botz</b>\n".format(
+        tmp = progress + "<b>⭕Completed:</b>{0} \n<b>⭕Total Size:</b> {1}\n<b>⭕Speed:</b> {2}/s\n<b>⭕ETA:</b> {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
