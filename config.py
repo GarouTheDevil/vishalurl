6a1c72ec4a634b8890d0eff33367abc7edc16a05
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-06-06 00:06:08 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+BOTTOKEN = os.environ.get("BOTTOKEN", "")
+APIID = int(os.environ.get("APIID", 12345))
+APIHASH = os.environ.get("APIHASH", "lelhutto0")
+DOWNLOADPATH = os.environ.get("DOWNLOADPATH", "Downloads/")
+USERNAME = os.environ.get("USERNAME", "")
