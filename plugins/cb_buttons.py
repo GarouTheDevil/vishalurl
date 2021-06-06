233590038b6b7118e30d0f7b83324de8af1fa6a7
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-06-06 10:56:17 +0530

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -49,9 +49,6 @@ async def formatbuttons(bot, update):
        
     if "|" in update.data:
         await zee5_execute(bot, update)
-        
-    elif "closeformat" in update.data:     
-        await update.message.delete()
 
 @pyrogram.Client.on_callback_query()
 async def button(bot, update):
