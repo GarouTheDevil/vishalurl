4a782d4106e9b6c1d7d48ffa6ea1ffb6b099db57
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-06-06 08:17:55 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BOTTOKEN = os.environ.get("BOTTOKEN", "")
-APIID = int(os.environ.get("APIID", 12345))
-APIHASH = os.environ.get("APIHASH", "lelhutto0")
-DOWNLOADPATH = os.environ.get("DOWNLOADPATH", "Downloads/")
-USERNAME = os.environ.get("USERNAME", "")
