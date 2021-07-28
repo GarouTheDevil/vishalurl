eb4f65ad21a93352b7bfbcdc1f1d1e232b1b4d90
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-07-28 12:37:14 +0530

    Create mega.py

---
+++ plugins/mega.py
@@ -0,0 +1,42 @@
+import os
+import logging
+from pyrogram import filters, Client, idle
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from mega import Mega
+from sample_config import Config
+
+# mega client
+mega = Mega()
+m = mega.login()
+
+# location
+LOCATION = "./"
+
+# logging
+bot = Client(
+   "MegaNz",
+   api_id=Config.API_ID,
+   api_hash=Config.API_HASH,
+   bot_token=Config.BOT_TOKEN,
+)
+
+# mega download
+bot.on_message(filters.command("mega") & filters.private)
+async def meganz(_, message):
+    input = message.text
+    user = message.from_user.mention
+    msg = await message.reply_text("**Downloading ⬇️**")
+    try:
+        file = m.download_url(input, LOCATION)
+    except Exception as e:
+        print(str(e))
+        return await msg.edit("**Send Mega Link**")
+    await msg.edit("**Uploading ⬆️**")
+    cap = f"**Uploaded By** :- {user} \n© @TheAllInOne_Robot"
+    await bot.send_document(message.chat.id, file, caption=cap)
+    await msg.delete()
+    os.remove(file)
+
+
+bot.start()
+idle()
