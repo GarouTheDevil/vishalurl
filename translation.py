de5b31fe8956c68c1247f2730e35d66a7ba9edab
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-08-27 01:02:23 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,3 @@
-import pyrogram
-from pyrogram import InlineKeyboardMarkup, InlineKeyboardButton
-
 class Translation(object):
     START_TEXT = """<b>Hi , Iam All In One Bot </b>
 """
@@ -10,27 +7,7 @@ class Translation(object):
     UPGRADE_TEXT = """
 **A Modified Multiple Functional Bot**
 """
-START_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('HELP', callback_data='help'),
-        InlineKeyboardButton('ABOUT', callback_data='about'),
-        InlineKeyboardButton('CLOSE', callback_data='close')
-        ]]
-    )
-    HELP_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('HOME', callback_data='home'),
-        InlineKeyboardButton('ABOUT', callback_data='about'),
-        InlineKeyboardButton('CLOSE', callback_data='close')
-        ]]
-    )
-    ABOUT_BUTTONS = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton('HOME', callback_data='home'),
-        InlineKeyboardButton('HELP', callback_data='help'),
-        InlineKeyboardButton('CLOSE', callback_data='close')
-        ]]
-    )
+
     DOWNLOAD_START = "<b>Downloading ⬇️</b>"
     UPLOAD_START = "<b>Uploading ⬆️</b>"
     AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Thank You For Using Me \n©️ @TheAllInOne_Robot</b>"
