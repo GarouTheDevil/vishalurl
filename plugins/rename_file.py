90586fc0bb18645be1c8926a168f32d8cb500efb
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-08-27 10:11:32 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -65,7 +65,7 @@ async def rename_doc(bot, update):
             await update.reply_text("Something Wrong. Contact my Support Group")
             return
     
-    TRChatBase(update.from_user.id, update.text, "rename")
+    #TRChatBase(update.from_user.id, update.text, "rename")
     if (" " in update.text) and (update.reply_to_message is not None):
         cmd, file_name = update.text.split(" ", 1)
         if len(file_name) > 64:
