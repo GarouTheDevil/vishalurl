6f81fbedc89d446032e30c494ae2a9aa864126ec
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-08-29 00:03:30 +0530

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -173,8 +173,6 @@ async def button(bot, update):
         try:
             await query.message.reply_to_message.delete()
 
-@Client.on_callback_query()
-async def button(bot, update):
     if "|" in update.data:
         await youtube_dl_call_back(bot, update)
     elif "=" in update.data:
