50c2e87fa69267bbb8059141f8505d77c5ca193a
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-08-29 21:55:40 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -54,8 +54,8 @@ async def help_user(bot, update):
                     InlineKeyboardButton('File To Link', callback_data = "f2l")
                 ],
                 [
-                    InlineKeyboardButton('File To File', callback_data = "f2f"),
-                    InlineKeyboardButton('Trim', callback_data = "trim")
+                    InlineKeyboardButton('Trim', callback_data = "trim"),
+                    InlineKeyboardButton('File To File', callback_data = "f2f")
                 ],
                 [
                     InlineKeyboardButton('Url Uploading', callback_data = "urlupload"),
