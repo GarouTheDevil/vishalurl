70c67000ae3da51c09fd0e5ada3917fd2c89bad4
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-09-06 23:24:52 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -75,7 +75,7 @@ async def rename_doc(bot, update):
             present_time = round(Config.PROCESS_MAX_TIMEOUT-(current_time - previous_time))
             Config.ADL_BOT_RQ[str(update.from_user.id)] = time.time()
             if round(current_time - previous_time) < Config.PROCESS_MAX_TIMEOUT:
-                await bot.edit_message_text(chat_id=update.chat.id, text=Translation.FREE_USER_LIMIT_Q_SZE.format(process_max_timeout, present_time), disable_web_page_preview=True, parse_mode="html", message_id=fmsg.message_id)
+                await bot.edit_message_text(chat_id=update.chat.id, text=Translation.FREE_USER_LIMIT_Q_SZE.format(process_max_timeout, present_time), disable_web_page_preview=True, parse_mode="html", message_id=update.message_id)
                 return
         else:
             Config.ADL_BOT_RQ[str(update.from_user.id)] = time.time()
