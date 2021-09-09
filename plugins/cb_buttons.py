17cece89e31034baf962d4af1fd4e7614caf37f6
David-raj123 <64373160+David-raj123@users.noreply.github.com>
2021-09-09 10:04:28 +0530

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -54,10 +54,10 @@ async def button(bot, update):
                 ],
                 [
                     InlineKeyboardButton('Url Uploading', callback_data = "urlupload"),
-                    InlineKeyboardButton('Thumbnail', callback_data = "about")
+                    InlineKeyboardButton('Thumbnail', callback_data = "cthumb")
                 ],
                 [
-                    InlineKeyboardButton('BACK', callback_data = "cthumb"),
+                    InlineKeyboardButton('BACK', callback_data = "ghelp"),
                     InlineKeyboardButton('ABOUT', callback_data = "about")
                 ]
             ]
